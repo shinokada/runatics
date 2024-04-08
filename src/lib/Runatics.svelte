668b72aa1ab270d90802ca5caec98be06855872f
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-08 08:48:03 +0200

    first commit

---
+++ src/lib/Runatics.svelte
@@ -0,0 +1,23 @@
+<script>
+	let { analyticsId } = $props();
+
+  $effect(() => {
+    const script = document.createElement('script');
+    script.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', '${analyticsId}');
+      `;
+    document.head.appendChild(script);
+  })
+</script>
+
+
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={analyticsId}"></script>
+</svelte:head>
+{#if !analyticsId}
+<h2>You need to provide your Google Analytics ID</h2>
+<p>Please read the docs how to set it up.</p>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<h1>Runatics</h1>
+
+<h2>Installation</h2>
+
+<p>Enables </p>
+<h2>How to set up Runatics.</h2>
+
